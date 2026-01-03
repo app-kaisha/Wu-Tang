f7b5cb59b4e605c589f62aaaa894f3f5cafdef09
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-03 14:48:19 +0000

    UI with images, text and buttons layout done

--- Wu-Tang/ContentView.swift
+++ Wu-Tang/ContentView.swift
@@ -9,14 +9,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var enteredName = ""
+    @State private var staticCoderNameIs = "Your Wu-Tang Coder Name is:"
+    @State private var wuTangName = "Undefined Breakpoint"
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Wu-Tang\nCoder Name Generator")
+                .font(.title)
+                .fontWeight(.black)
+                .foregroundStyle(.yellow)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.horizontal)
+                .padding(.bottom)
+                .background(.black)
+            
+            Spacer()
+            TextField("Enter name here", text: $enteredName)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.gray, lineWidth: 1)
+                }
+                .padding()
+            Button {
+                
+            } label: {
+                    Image("wu-tang-button")
+                    Text("Get It!")
+
+                
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2).bold()
+            .foregroundStyle(.yellow)
+            .tint(.black)
+            
+            VStack{
+                Text(staticCoderNameIs)
+                    .multilineTextAlignment(.leading)
+                    
+                Text(wuTangName)
+                    .fontWeight(.black)
+                    .multilineTextAlignment(.center)
+            }
+            .font(.largeTitle)
+            .frame(height: 130)
+            .minimumScaleFactor(0.5)
+            
+            
+            Spacer()
+            
+            Image("wu-tang")
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
     }
 }
 
