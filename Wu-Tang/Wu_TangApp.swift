c3dc8002b3c9d860d305e6ac7e81a20448a9b19c
app-kaisha <app.kaihatsusha@gmail.com>
2026-01-03 12:41:33 +0000

    Initial Commit

---
+++ Wu-Tang/Wu_TangApp.swift
@@ -0,0 +1,18 @@
+//
+//  Wu_TangApp.swift
+//  Wu-Tang
+//
+//  Created by app-kaihatsusha on 03/01/2026.
+//  Copyright © 2026 app-kaihatsusha. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Wu_TangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
